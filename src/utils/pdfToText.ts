6001bf1072def5f365192c71b4132b8659f43c01
utku-guclu <utku_guclu@hotmail.com>
2024-10-07 16:01:14 +0300

    array buff

--- src/utils/pdfToText.ts
+++ src/utils/pdfToText.ts
@@ -1,10 +1,23 @@
-import pdfParse from "pdf-parse";
+import * as pdfjsLib from "pdfjs-dist";
 
-export const pdfToText = async (file: File): Promise<string> => {
+// You may need to set the workerSrc to allow PDF.js to work properly
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+
+export async function pdfToText(file: File) {
   const arrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  const pdfData = await pdfParse(buffer);
+  const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
 
-  // The extracted text will be available in pdfData.text
-  return pdfData.text;
-};
+  let fullText = "";
+  const numPages = pdf.numPages;
+
+  for (let i = 1; i <= numPages; i++) {
+    const page = await pdf.getPage(i);
+    const textContent = await page.getTextContent();
+
+    // Extract text
+    const pageText = textContent.items.map((item: any) => item.str).join(" ");
+    fullText += pageText + "\n";
+  }
+
+  return fullText;
+}
